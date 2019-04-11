1817daaa0c0be6aeb6ead676953ab2802170f7bb
memek123456 <46232817+memek123456@users.noreply.github.com>
2019-04-11 17:33:54 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// configurasi //
+
+
+// user_name boleh kosong //
+$user_name="";
+
+//user_gaid wajib isi
+$user_gaid= "4cc13665-8942-4a6a-b010-6eab3448341b";
+//user_token wajib di isi //
+$user_token="AbycwUZW5vcAvJ7t";
+
+//user_id wajib di isi //
+$user_id="Facebook_771905749858288";
+
+// invite_code wajib di isi //
+$invite_code="F3XBpgLVho";
+
+// Bearer wajib di isi //
+$Bearer="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJGYWNlYm9va183NzE5MDU3NDk4NTgyODgiLCJleHAiOiIxNTU3NDIyMTAxNzg2In0.jLsqcJtykr093lZkY3QcOR576MPcgHHa5kUOVaYn7w4";
+
+
+
+?>
